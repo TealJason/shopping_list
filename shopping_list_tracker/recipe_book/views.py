da8ecd71ccbd00c621b4011d9a8ad2126652dacf
TealJason <jason.beard@phe.gov.uk@phe-pf12qyrt.eud.unix.phe.gov.uk>
2026-01-05 14:15:14 +0000

    add basic rendering for the recipe book

--- shopping_list_tracker/recipe_book/views.py
+++ shopping_list_tracker/recipe_book/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 from . import utils
 from django.http import request
+import json
+import os
+from django.conf import settings
 
 # Create your views here.
 def recipe_book(request): 
-    return render(request, "recipe_book.html")
+    
+    path = os.path.join(settings.BASE_DIR, "recipe_book", "data", f"recipe_book.json")
+
+    with open(path) as f:
+        recipes = json.load(f)
+            
+    return render(request, "recipe_book.html",{'recipes': recipes})
 
 def addRecipe(request):
     if request.method == "POST":
         ingredients_list = request.POST["ingredients"].split(",")
         recipe_name = request.POST["recipename"].strip()
         utils.addRecipe(ingredients_list, recipe_name)
-    return render(request, "recipe_book.html")
+        path = os.path.join(settings.BASE_DIR, "recipe_book", "data", f"recipe_book.json")
+
+        with open(path) as f:
+            recipes = json.load(f)
+
+        return render(request, "recipe_book.html", {'recipes': recipes})
