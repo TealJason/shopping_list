99d202aa36000698770c502f8790c4050d6a3846
TealJason <jason.beard@phe.gov.uk@phe-pf12qyrt.eud.unix.phe.gov.uk>
2026-03-17 15:26:40 +0000

    add button to remove items from fridge

--- shopping_list_tracker/current_meals/views.py
+++ shopping_list_tracker/current_meals/views.py
@@ -17,21 +17,34 @@ def current_meals(request):
         "fridge_items": fridge_items,
         "possible_meals": possible_meals
     })
-
+    
 def update_or_check_fridge(request):
     fridge_path = os.path.join(settings.BASE_DIR, "data", "fridge.txt")
 
     if request.method == "POST":
+        # NEW: get updated fridge list
+        current_items = request.POST.getlist("current_items")
+        current_items = [i.strip().lower() for i in current_items if i.strip()]
+
+        # overwrite fridge with new list
+        with open(fridge_path, "w") as f:
+            for item in sorted(set(current_items)):
+                f.write(item + "\n")
+
+        # OPTIONAL: still allow adding new ingredients
         ingredients = request.POST.getlist("ingredients")
         ingredients = [i.strip().lower() for i in ingredients if i.strip()]
-
-        remove = request.POST.get("remove") == "1"
-
-        utils.write_fridge_csv(ingredients, remove)
+        if ingredients:
+            utils.write_fridge_csv(ingredients, remove=False)
 
     fridge_items = []
     if os.path.exists(fridge_path):
         with open(fridge_path) as f:
             fridge_items = [line.strip() for line in f]
+
     possible_meals = utils.find_possible_meals(fridge_items)
-    return render(request, "current_meals.html", {"fridge_items": fridge_items,"possible_meals":possible_meals})
+
+    return render(request, "current_meals.html", {
+        "fridge_items": fridge_items,
+        "possible_meals": possible_meals
+    })
